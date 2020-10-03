977f882ad3f7afbbca33a00cf7c1540a176d4933
WillRock19 <williamporto_14@hotmail.com>
2020-10-03 19:22:58 -0300

    using parse-opts to get arguments instead of do it manually

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -1,4 +1,5 @@
 (ns conversor.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
 (defn- valores-em 
@@ -10,9 +11,16 @@
       {:para (.substring argumento 7)}
     :else {}))
 
+  (def opcoes-aceitaveis [
+    ["-d", "--de moeda base", "moeda base para conversão"]
+    ["-p", "--para moeda desejada", "moeda a qual queremos efetuar a conversão"]
+  ])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Iniciando processamento...")
   (println "Temos" (count args) "argumentos.")
-  (println "Os argumentos são:" (map valores-em args)))
+  (let [estrutura (parse-opts args opcoes-aceitaveis)]
+    (println "Estrutura criada: " estrutura)
+    (println "Opções: " (:options estrutura))))
