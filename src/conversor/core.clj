713cbaf69fc355102c1781fbbf57bbf0a66f7eed
WillRock19 <williamporto_14@hotmail.com>
2020-10-03 19:46:30 -0300

    Refactored for legibility

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -2,19 +2,11 @@
   (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(defn- valores-em 
-  [argumento]
-  (cond 
-    (.startsWith argumento "--de=")
-      {:de (.substring argumento 5)}
-    (.startsWith argumento "--para=")
-      {:para (.substring argumento 7)}
-    :else {}))
 
-  (def opcoes-aceitaveis [
-    ["-d", "--de moeda base", "moeda base para conversão"]
-    ["-p", "--para moeda desejada", "moeda a qual queremos efetuar a conversão"]
-  ])
+(def opcoes-aceitaveis [
+  ["-d", "--de moeda base", "moeda base para conversão"]
+  ["-p", "--para moeda desejada", "moeda a qual queremos efetuar a conversão"]
+])
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -22,5 +14,4 @@
   (println "Iniciando processamento...")
   (println "Temos" (count args) "argumentos.")
   (let [estrutura (parse-opts args opcoes-aceitaveis)]
-    (println "Estrutura criada: " estrutura)
     (println "Opções: " (:options estrutura))))
