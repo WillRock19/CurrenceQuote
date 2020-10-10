0c9f932e2e7fedd9e4808452fb34aa28ebf4da96
WillRock19 <williamporto_14@hotmail.com>
2020-10-10 09:03:06 -0300

    Getting coin price from API and showing it to the user

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -18,6 +18,16 @@
   ["-p", "--para moeda desejada", "moeda a qual queremos efetuar a conversão"]
 ])
 
+
+(defn obter-cotacao [de para]
+  (let [moedas-formato-api (parametrizar-moedas de para)]
+    (-> (:body (http/get api-url {:query-params { "q" moedas-formato-api, "apiKey" chave-api }}))
+        (parse-string)
+        (get-in ["results" moedas-formato-api "val"]))))
+
+(defn formata-resposta [valor-cotacao moeda-original moeda-convertida]
+  (str "1 " moeda-original " equivale a " valor-cotacao " em " moeda-convertida))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -26,4 +36,6 @@
   (let [estrutura (parse-opts args opcoes-aceitaveis)
         {:keys [de para]} (:options estrutura)]
     (prn "Opções:"  (:options estrutura))
-    (prn "Chave da api:" chave-api)))
+    (-> (obter-cotacao de para)
+        (formata-resposta de para)
+        (prn))))
